81a9c7db0ead6c9735f8c04f70603ab81fedcb7c
Jennifer Pazos <jmp521@gmail.com>
2018-05-13 01:53:43 -0500

    added standard crud methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,34 @@
 class OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(order_params)
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+    @order = Order.find(order_params)
+  end
+
+  def destroy
+    if @order
+      @order.destroy
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:orders).permit(:status, :customer_id)
+  end
 end
