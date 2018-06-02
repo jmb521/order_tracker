34c5c4d13fc48375b2b680a066748ee86572968a
Jennifer Pazos <jmp521@gmail.com>
2018-06-02 17:04:33 -0500

    added customers_controller and methods for routes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,32 @@
 class CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+
+    @customer = Customer.create(customer_params)
+    redirect_to customer_path(@customer)
+  end
+
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :phone)
+  end
 end
