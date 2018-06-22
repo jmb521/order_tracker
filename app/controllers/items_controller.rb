5c23eee5f3e4a96c02351c2922de47d07eab31f2
Jennifer Pazos <jmp521@gmail.com>
2018-06-22 15:18:42 -0500

    added crud methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,34 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.create(order_params)
+  end
+
+  def new
+    
+
+  end
+
+  def edit
+    @item = Item.find(order_params)
+  end
+
+  def destroy
+    if @item
+      @item.destroy
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:orders).permit(:status, :customer_id)
+  end
 end
